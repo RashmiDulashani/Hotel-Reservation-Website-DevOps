a7b060a8af842248994f17c3103c59a4d1f60736
Imashi <imashidilshani@gmail.com>
2026-01-24 11:24:12 +0530

    feat: Add User Registration Functionality with Database Integration

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -41,3 +41,93 @@
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 
+<!-- Navigation Bar Active -->
+<script>
+
+    function setActive()
+    {
+        let navbar = document.getElementById("nav-bar");
+        let a_tags = navbar.getElementsByTagName("a");
+
+        for(let i=0; i<a_tags.length; i++)
+        {
+            let file = a_tags[i].href.split("/").pop();
+            let file_name = file.split(".")[0];
+
+            if(document.location.href.indexOf(file_name) >= 0) {
+                a_tags[i].classList.add("active");
+            }
+        }
+    }
+
+    function alert(type, msg, position='body') {
+        let bs_class = (type == "success") ? "alert-success" : "alert-danger";
+        let element = document.createElement("div");
+        element.innerHTML = `
+            <div class="alert alert ${bs_class} alert-dismissible fade show" role="alert">
+                <strong class="me-3">${msg}</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `;
+
+        if(position=='body') {
+            document.body.append(element);
+            element.classList.add('custom-alert');
+        }
+        else {
+            document.getElementById(position).appendChild(element);
+        }
+
+        setTimeout(remAlert, 3000);
+    }
+
+    function remAlert() {
+        document.getElementsByClassName('alert')[0].remove();
+    }
+
+    let register_form = document.getElementById('register-form');
+
+    register_form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let data = new FormData();
+
+        data.append('name', register_form.elements['name'].value);
+        data.append('email', register_form.elements['email'].value);
+        data.append('phonenum', register_form.elements['phonenum'].value);
+        data.append('address', register_form.elements['address'].value);
+        data.append('dob', register_form.elements['dob'].value);
+        data.append('pass', register_form.elements['pass'].value);
+        data.append('cpass', register_form.elements['cpass'].value);
+        data.append('register', '');
+
+        var myModal = document.getElementById("registerModal")
+        var modal = bootstrap.Modal.getInstance(myModal);
+        modal.hide();
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "ajax/login_register.php", true);
+
+        xhr.onload = function() {
+            if(this.responseText == 'pass_mismatch') {
+                alert('error', "Password Mismatch!");
+            }
+            else if(this.responseText == 'email_already') {
+                alert('error', "Email is already registered!");
+            }
+            else if(this.responseText == 'phone_already') {
+                alert('error', "Phone Number is already registered!");
+            }
+            else if(this.responseText == 'ins_failed') {
+                alert('error', "Registration Failed! Server Down!");
+            }
+            else {
+                alert('success', "Registration Successful!");
+                register_form.reset();
+            }
+        }
+        
+        xhr.send(data);
+    });
+
+</script>
